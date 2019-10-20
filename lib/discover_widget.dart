907f145ac8251a8b5318985b7a0776585e42e4a6
YNVoon <voonyn@gmail.com>
2019-10-20 20:54:43 +0800

    discover page ui completed

--- lib/discover_widget.dart
+++ lib/discover_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DiscoverWidget extends StatefulWidget {
   DiscoverWidget({Key key}) : super(key: key);
@@ -7,6 +8,25 @@ class DiscoverWidget extends StatefulWidget {
 }
 
 class _DiscoverWidgetState extends State<DiscoverWidget> {
+
+  var shoppingListTitle = ["Master Card", "Union Pay"];
+  var shoppingListDescription = ["Pay master card bill", "Pay Union Pay bill"];
+  var blockChainListTitle = ["BTC", "ETH", "AES", "USDT"];
+  var blockChainListWebPath = [
+    "https://coinmarketcap.com/currencies/bitcoin/",
+    "https://coinmarketcap.com/currencies/ethereum/",
+    "https://coinmarketcap.com/currencies/tether/",
+    "https://coinmarketcap.com/currencies/tether/"
+  ];
+
+  void _launchURL(String path) async {
+    var url = path;
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch URL';
+    }
+  }
 
   void _showMaterialDialog() {
     showDialog(
@@ -22,177 +42,325 @@ class _DiscoverWidgetState extends State<DiscoverWidget> {
       }
     );
   }
-  @override
-  Widget build(BuildContext context) {
-    return CustomScrollView(
-      slivers: <Widget>[
-        SliverToBoxAdapter(
+
+  Widget _buildCard(String imagePath, int index) {
+    return Container(
+      margin: index % 2 == 0 ? EdgeInsets.only(left: 15.0) : EdgeInsets.only(right: 15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(const Radius.circular(10.0)),
+        color: Colors.white,
+      ),
+      width: MediaQuery.of(context).size.width / 2,
+      // height: 85.0,
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            _launchURL(blockChainListWebPath[index]);
+          },
           child: Container(
-            decoration: BoxDecoration(
-              color: Color(0xFF323c83),
-              borderRadius: BorderRadius.all(const Radius.circular(10.0))
-            ),
-            height: 150.0,
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 20.0, bottom: 10.0),
-            child: Column(
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Text(
-                  "AES",
-                  style: TextStyle(
-                    color: Colors.white, 
-                    fontSize: 60.0,
-                    letterSpacing: 5.0
-                  ),
-                ),
-                Text(
-                  "EXCHANGE",
-                  style: TextStyle(
-                    color: Colors.white, 
-                    fontSize: 16.0,
-                    letterSpacing: 5.0
+                Text("T", style: TextStyle(fontSize: 30.0)),
+                // Image(
+                //   image: AssetImage(imagePath),
+                // ),
+                Container(
+                  margin: EdgeInsets.only(left: 20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 120.0,
+                        child: Text(blockChainListTitle[index] + " Explorer", textAlign: TextAlign.start, style: TextStyle(fontWeight: FontWeight.bold),),
+                      ),
+                      Container(
+                        width: 120.0,
+                        child: Text("Browse the " + blockChainListTitle[index] + " latest info", style: TextStyle(fontSize: 12.0, color: Colors.grey), textAlign: TextAlign.start)
+                      )
+                    ],
                   ),
                 )
               ],
             ),
           ),
         ),
-        SliverToBoxAdapter(
+      ),
+    );
+
+  }
+
+  Widget _buildShoppingCard(String imagePath, int index) {
+    return Container(
+      margin: index % 2 == 0 ? EdgeInsets.only(left: 15.0) : EdgeInsets.only(right: 15.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(const Radius.circular(10.0)),
+        color: Colors.white,
+      ),
+      width: MediaQuery.of(context).size.width / 2,
+      // height: 85.0,
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            _showMaterialDialog();
+          },
           child: Container(
-            width: MediaQuery.of(context).size.width,
-            margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 10.0),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                Flexible(
-                  flex: 1,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        ClipOval(
-                          child: Material(
-                            color: Color(0xFFefefed),
-                            child: InkWell(
-                              splashColor: Colors.grey,
-                              child: SizedBox(
-                                width: 56.0,
-                                height: 56.0,
-                                child: Icon(Icons.toys),                            
-                              ),
-                              onTap: () {
-                                _showMaterialDialog();
-                              },
-                            ),
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(top: 5.0),
-                          child: Text(
-                            "Convenient",
-                            style: TextStyle(
-                              fontSize: 12.0
-                            ),
-                          ) ,
-                        )
-                      ],
-                    ),
-                  )
-                ),
-                Flexible(
-                  flex: 1,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        ClipOval(
-                          child: Material(
-                            color: Color(0xFFefefed),
-                            child: InkWell(
-                              splashColor: Colors.grey,
-                              child: SizedBox(
-                                width: 56.0,
-                                height: 56.0,
-                                child: Icon(Icons.shopping_cart),                            
-                              ),
-                              onTap: () {
-                                _showMaterialDialog();
-                              },
-                            ),
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(top: 5.0),
-                          child: Text(
-                            "Shopping center",
-                            style: TextStyle(
-                              fontSize: 12.0
-                            ),
-                          ) ,
-                        )
-                      ],
-                    ),
-                  )
-                ),
-                Flexible(
-                  flex: 1,
-                  child: Container(
-                    width: MediaQuery.of(context).size.width,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        ClipOval(
-                          child: Material(
-                            color: Color(0xFFefefed),
-                            child: InkWell(
-                              splashColor: Colors.grey,
-                              child: SizedBox(
-                                width: 56.0,
-                                height: 56.0,
-                                child: Icon(Icons.ondemand_video),                            
-                              ),
-                              onTap: () {
-                                _showMaterialDialog();
-                              },
-                            ),
-                          ),
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(top: 5.0),
-                          child: Text(
-                            "Webcast",
-                            style: TextStyle(
-                              fontSize: 12.0
-                            ),
-                          ) ,
-                        )
-                      ],
-                    ),
-                  )
-                ),
+                Text("T", style: TextStyle(fontSize: 30.0)),
+                // Image(
+                //   image: AssetImage(imagePath),
+                // ),
+                Container(
+                  margin: EdgeInsets.only(left: 20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                        width: 120.0,
+                        child: Text(shoppingListTitle[index], textAlign: TextAlign.start, style: TextStyle(fontWeight: FontWeight.bold),),
+                      ),
+                      Container(
+                        width: 120.0,
+                        child: Text(shoppingListDescription[index], style: TextStyle(fontSize: 12.0, color: Colors.grey), textAlign: TextAlign.start)
+                      )
+                    ],
+                  ),
+                )
               ],
             ),
           ),
         ),
-        SliverToBoxAdapter(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.only(left: 12.0, bottom: 15.0),
-            margin: EdgeInsets.only(top: 25.0),
-            child: Text(
-              "Shopping Center",
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
-              textAlign: TextAlign.start,
+      ),
+    );
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverToBoxAdapter(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color(0xFF323c83),
+                borderRadius: BorderRadius.all(const Radius.circular(10.0))
+              ),
+              height: 150.0,
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 20.0, bottom: 10.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "AES",
+                    style: TextStyle(
+                      color: Colors.white, 
+                      fontSize: 60.0,
+                      letterSpacing: 5.0
+                    ),
+                  ),
+                  Text(
+                    "EXCHANGE",
+                    style: TextStyle(
+                      color: Colors.white, 
+                      fontSize: 16.0,
+                      letterSpacing: 5.0
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
-        )
-      ],
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              margin: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 10.0),
+              child: Row(
+                children: <Widget>[
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          ClipOval(
+                            child: Material(
+                              color: Color(0xFFefefed),
+                              child: InkWell(
+                                splashColor: Colors.grey,
+                                child: SizedBox(
+                                  width: 56.0,
+                                  height: 56.0,
+                                  child: Icon(Icons.toys),                            
+                                ),
+                                onTap: () {
+                                  _showMaterialDialog();
+                                },
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 5.0),
+                            child: Text(
+                              "Convenient",
+                              style: TextStyle(
+                                fontSize: 12.0
+                              ),
+                            ) ,
+                          )
+                        ],
+                      ),
+                    )
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          ClipOval(
+                            child: Material(
+                              color: Color(0xFFefefed),
+                              child: InkWell(
+                                splashColor: Colors.grey,
+                                child: SizedBox(
+                                  width: 56.0,
+                                  height: 56.0,
+                                  child: Icon(Icons.shopping_cart),                            
+                                ),
+                                onTap: () {
+                                  _showMaterialDialog();
+                                },
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 5.0),
+                            child: Text(
+                              "Shopping center",
+                              style: TextStyle(
+                                fontSize: 12.0
+                              ),
+                            ) ,
+                          )
+                        ],
+                      ),
+                    )
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          ClipOval(
+                            child: Material(
+                              color: Color(0xFFefefed),
+                              child: InkWell(
+                                splashColor: Colors.grey,
+                                child: SizedBox(
+                                  width: 56.0,
+                                  height: 56.0,
+                                  child: Icon(Icons.ondemand_video),                            
+                                ),
+                                onTap: () {
+                                  _showMaterialDialog();
+                                },
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 5.0),
+                            child: Text(
+                              "Webcast",
+                              style: TextStyle(
+                                fontSize: 12.0
+                              ),
+                            ) ,
+                          )
+                        ],
+                      ),
+                    )
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.only(left: 12.0, bottom: 20.0),
+              margin: EdgeInsets.only(top: 25.0),
+              child: Text(
+                "Shopping Center",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+                textAlign: TextAlign.start,
+              ),
+            ),
+          ),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: MediaQuery.of(context).size.width / 2,
+              mainAxisSpacing: 15.0,
+              crossAxisSpacing: 15.0,
+              childAspectRatio: 2.0
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return _buildShoppingCard("testing", index);
+              },
+              childCount: 2
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.only(left: 12.0, bottom: 20.0),
+              margin: EdgeInsets.only(top: 25.0),
+              child: Text(
+                "Blockchain",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),
+                textAlign: TextAlign.start,
+              ),
+            ),
+          ),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: MediaQuery.of(context).size.width / 2,
+              mainAxisSpacing: 15.0,
+              crossAxisSpacing: 15.0,
+              childAspectRatio: 2.0
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return _buildCard("testing", index);
+              },
+              childCount: 4
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 20.0,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
